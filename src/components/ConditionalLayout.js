a78d98777caedddd2fea86f28201e7c8302b8b73
Wenuri Sanjana <wenuris2004@gmail.com>
2026-07-07 23:19:45 +0530

    added

---
+++ src/components/ConditionalLayout.js
@@ -0,0 +1,18 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+export default function ConditionalLayout({ children }) {
+  const pathname = usePathname();
+  const isAdmin = pathname?.startsWith("/admin");
+
+  return (
+    <>
+      {!isAdmin && <Header />}
+      <main className="flex-grow">{children}</main>
+      {!isAdmin && <Footer />}
+    </>
+  );
+}
